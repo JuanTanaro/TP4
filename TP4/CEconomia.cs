7e25ae6c7a5b441a0c4ebdf2ce747137185b8482
Juan Matias Tulli <jmatiastulli@gmail.com>
2021-05-30 20:19:14 -0300

    Changes

--- TP4/CEconomia.cs
+++ TP4/CEconomia.cs
@@ -8,6 +8,26 @@ namespace TP4
 {
     class CEconomia
     {
+        public int CodigoMateria { get; set; }
+        public string NombreMateria { get; set; }
+
+        public CEconomia() { }
+
+        public CEconomia(string linea)
+        {
+            var datos = linea.Split('-');
+            CodigoMateria = int.Parse(datos[0]);
+            NombreMateria = string.Format(datos[1]);
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Codigo de Materia {CodigoMateria}");
+            Console.WriteLine($"Nombre de Materia {NombreMateria}");
+            Console.WriteLine();
+        }
+
 
     }
 }
